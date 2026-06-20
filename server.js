efb391bf83e2aa4f2bc529f8a248ae21821af68b
lp7554499 <lp7554499@gmail.com>
2026-06-20 23:23:50 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const app = require('./src/app')
+const connectToDb = require('./config/database')
+
+connectToDb()
+
+app.listen(3000,()=>{
+    console.log("server are runnig in port 3000")
+})
