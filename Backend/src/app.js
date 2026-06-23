99796b158241083ccb09324500254c3d92d522e2
lp7554499 <lp7554499@gmail.com>
2026-06-23 11:03:12 +0530

    cors add

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,11 @@ const app = express()
 app.use(express.json())
 const cookieParser = require('cookie-parser')
 app.use(cookieParser())
+const cors = require('cors')
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials:true
+}))
 
 // require all route related to auth
 const authRouter = require('./routes/auth.route')
