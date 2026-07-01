a9a139bf48db2852f9f80a71bfddaf4acab4a982
lp7554499 <lp7554499@gmail.com>
2026-07-01 12:38:57 +0530

    fix: add vercel URL to CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 app.use(cors({
    origin: [
         'http://localhost:5173',
-        'https://prep-ai-frontend.onrender.com'
+        'https://prep-ai-navy-nine.vercel.app'
     ],
     
     credentials:true
